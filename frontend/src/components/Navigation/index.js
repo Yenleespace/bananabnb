ad454650e38625e969f31f16a70780485df2f112
Yen Lee <yenny0104@gmail.com>
2023-04-06 09:56:51 -0400

    greenlit

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -46,9 +46,9 @@ function Navigation() {
               <p className='main-title'>BananaBnB</p>
             </div>
           </NavLink>
-          <form className="d-flex" role="search">
+          {/* <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          </form>
+          </form> */}
           <ul className="navbar-nav mb-2 mb-lg-0">
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
